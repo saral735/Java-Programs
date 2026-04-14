2c3592e37b49c5099e25e74792f381c25effd758
saral735 <kushsaral34@gmail.com>
2026-04-14 16:09:08 +0530

    added D class in variable folder

---
+++ OOPS/variable/D.java
@@ -0,0 +1,17 @@
+package Variable;
+
+class D{
+	static int a;
+	int b;
+
+	public static void main(String[] args) {
+		int c;
+		System.out.println(D.a);
+		D d=new D();
+		System.out.println(d.b);
+//		System.out.println(c);
+	}
+}
+
+	
+
